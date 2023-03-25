4b90588e0477015d3f073df5c3cbd4df018e2e10
H. Felix Wittmann <hfwittmann@gmail.com>
2023-03-26 00:39:31 +0100

    changes: - added schedule

---
+++ backend/myschedule.py
@@ -0,0 +1,21 @@
+import schedule
+import time
+import subprocess
+import sys
+
+
+def job():
+    print("I'm working...")
+    subprocess.run(
+        ["kedro", "run"],
+        cwd="./kedro-backend",
+    )
+
+
+schedule.every(10).seconds.do(job)
+# schedule.every().day.at("09:00").do(job)
+
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
